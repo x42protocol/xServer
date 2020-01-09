10853a6209c03dd673cefe025956b559e3d57a8f
Dennis <41244965+DennisAMenace@users.noreply.github.com>
2020-01-09 13:43:33 -0700

    Made server ID URL safe.

--- x42-Server.UI/src/app/shared/models/serveridresponse.ts
+++ x42-Server.UI/src/app/shared/models/serveridresponse.ts
@@ -3,10 +3,35 @@ export class ServerIDResponse {
   public serverId: string = '';
 
   public setServerId(address: string) {
-    this.serverId = "SID" + btoa(address);
+    this.serverId = "SID" + this.Base64EncodeUrl(btoa(address));
   }
 
   public getAddressFromServerId() {
-    return atob(this.serverId.substring(3));
+    return this.Base64DecodeUrl(atob(this.serverId.substring(3)));
+  }
+
+  /**
+  * use this to make a Base64 encoded string URL friendly,
+  * i.e. '+' and '/' are replaced with '-' and '_' also any trailing '='
+  * characters are removed
+  *
+  * @param {String} str the encoded string
+  * @returns {String} the URL friendly encoded String
+  */
+  private Base64EncodeUrl(str) {
+    return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '');
+  }
+
+  /**
+   * Use this to recreate a Base64 encoded string that was made URL friendly 
+   * using Base64EncodeurlFriendly.
+   * '-' and '_' are replaced with '+' and '/' and also it is padded with '+'
+   *
+   * @param {String} str the encoded string
+   * @returns {String} the URL friendly encoded String
+   */
+  private Base64DecodeUrl(str) {
+    str = (str + '===').slice(0, str.length + (str.length % 4));
+    return str.replace(/-/g, '+').replace(/_/g, '/');
   }
 }
