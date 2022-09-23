38c64688e49dc8be65c517e0a2256bfba03fc5b9
Panayiotis Savva <psavva@gmail.com>
2022-09-23 21:47:18 +0300

    Metrics Feature

---
+++ xServer.D/Feature/Metrics/IRuntimeInformationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x42.Feature.Metrics
+{
+    public interface IRuntimeInformationService
+    {
+        string GetFrameworkDescription();
+        Architecture GetOSArchitecture();
+        string GetOSDescription();
+        OSPlatform GetOsPlatform();
+        Architecture GetProcessArchitecture();
+        string GetRuntimeIdentifier();
+        bool IsFreeBSD();
+        bool IsLinux();
+        bool IsOSX();
+        bool IsUnix();
+        bool IsWindows();
+    }
+}
